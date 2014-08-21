09bb45c01b188ddd09d65d90984cda745467f617
markableidinger <mark.ableidinger@gmail.com>
2014-08-21 09:28:57 -0700

    adds swap function

--- sortAnimate.js
+++ sortAnimate.js
@@ -1,21 +1,21 @@
-var deck=[]
-var solved=[]
-var started=false
-var interval
-var moves=0
+var deck = [];
+var solved = [];
+var started = false;
+var interval;
+var moves = 0;
 
 function createDeck(list){
-  for (i=1; i<=52; i++){
-    list[i-1]=i
+  for (i = 1; i <= 52; i++){
+    list[i - 1] = i;
   }
 }
 
-createDeck(deck)
-createDeck(solved)
+createDeck(deck);
+createDeck(solved);
 
 function shuffle(array){
-  m=array.length;
-  while (m>0) {
+  m = array.length;
+  while (m > 0) {
     rand = Math.floor(Math.random() * m--);
     t = array[m];
     array[m] = array[rand];
@@ -25,35 +25,44 @@ function shuffle(array){
 }
 
 function displayDeck(){
-  $('.container .card-container').html('')
-  for(var i=0; i<deck.length; i++){
-    $('.container .card-container').append('<img src="cardPictures/'+deck[i]+'.gif" id="'+deck[i]+'" value="'+i+'" class="card">')
+  $('.container .card-container').html('');
+  for(var i = 0; i < deck.length; i++){
+    $('.container .card-container').append('<img src="cardPictures/' + deck[i] + '.gif" id="' + deck[i] + '" value="' + i + '" class="card">');
   }
 }
-shuffle(deck)
-displayDeck()
+shuffle(deck);
+displayDeck();
 
 function bogo(){
-  if(deck==solved){
+  if(deck == solved){
     window.clearInterval(interval);
-    alert('DONE!')
+    alert('DONE!');
   }
   else{
-    shuffle(deck)
-    displayDeck()
+    shuffle(deck);
+    displayDeck();
   }
+}
+
+function swap(a, b){
+  // $('.card-container img:nth-child(' + a + ')').fadeOut(20);
+  // $('.card-container img:nth-child(' + b + ')').fadeOut(20, function(){
+    $('.card-container img:nth-child(' + a + ')').after($('.card-container img:nth-child(' + b + ')'));
+  // })
+  // $('.card-container img:nth-child(' + a + ')').fadeIn(20);
+  // $('.card-container img:nth-child(' + b + ')').fadeIn(20);
 }
 
 function insertion(){
-  moves=0
-  for (pivot=2; pivot<=52; pivot++){
-    change=0
-      while(parseInt($('.card-container img:nth-child('+(pivot-change)+')').attr('id'))<parseInt($('.card-container img:nth-child('+(pivot-change-1)+')'
+  moves = 0;
+  for (pivot = 2; pivot <= 52; pivot++){
+    change = 0;
+      while(parseInt($('.card-container img:nth-child(' + (pivot - change) + ')').attr('id')) < parseInt($('.card-container img:nth-child(' + (pivot - change - 1) + ')'
         ).attr('id'))){
-          $('.card-container img:nth-child('+(pivot-change)+')').delay(50).after($('.card-container img:nth-child('+(pivot-change-1)+')'))
-          change++
-          moves++
-          $('#moves').html('Moves: '+moves)
+          swap(pivot - change, pivot-change-1);
+          change++;
+          moves++;
+          $('#moves').html('Moves: ' + moves);
         }
       }
     }
@@ -61,24 +70,24 @@ function insertion(){
 $('#bogo').click(function(){
   if(started){
     window.clearInterval(interval);
-    $(this).text('BOGO')
-    started = false
+    $(this).text('BOGO');
+    started = false;
   }
   else{
-    started = true
-    interval = window.setInterval(function(){bogo()}, 40)
-    $(this).text('STOP')
+    started = true;
+    interval = window.setInterval(function(){bogo()}, 40);
+    $(this).text('STOP');
 }
 });
 
 $('#insertion').click(function(){
   if(started){
     window.clearInterval(interval);
-    $(this).text('INSERTION')
-    started = false
+    $(this).text('INSERTION');
+    started = false;
   }
   else{
-    started = true
-    insertion()
+    started = true;
+    insertion();
   }
 });
